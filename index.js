58fce3f0058c292411b9b5b9401d4d757064b53c
isir10 <isir.malak@moringaschool.com>
2022-08-28 15:21:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+  function superbowlWin(record) {
+	const object = record.find(function (object) {
+		return object.result === "W";
+	});
+	if (typeof object === "undefined") {
+		return undefined;
+	} else {
+		return object.year;
+	}
+}
